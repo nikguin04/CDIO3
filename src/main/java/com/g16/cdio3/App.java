516098e7ae235dbed4744c605a2bc1ab4a94bbf9
Niklas Jensen <niklaspj2004@gmail.com>
2023-11-03 08:34:20 +0100

    Maven initial project and test

---
+++ src/main/java/com/g16/cdio3/App.java
@@ -0,0 +1,13 @@
+package com.g16.cdio3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
