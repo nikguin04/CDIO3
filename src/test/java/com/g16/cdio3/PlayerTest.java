54be9c41f548878645868be2ca271bb7c41964df
Anders Greve Sørensen <anders1709@gmail.com>
2023-11-16 19:03:38 +0100

    Create test for buyProperty method

--- src/test/java/com/g16/cdio3/PlayerTest.java
+++ src/test/java/com/g16/cdio3/PlayerTest.java
@@ -1,12 +1,12 @@
 package com.g16.cdio3;
+
 import com.g16.cdio3.Player;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class PlayerTest {
-    
+
     @Test
     public void PlayerCreation() {
         Player player = new Player("NameTest", 100);
@@ -40,20 +40,19 @@ public class PlayerTest {
         Assert.assertEquals(false, player.AddToPosition(5));
         Assert.assertEquals(5, player.GetPosition());
 
-        //loops over to 0 at 24
+        // loops over to 0 at 24
         Assert.assertEquals(true, player.AddToPosition(19));
         Assert.assertEquals(0, player.GetPosition());
-        
+
         Assert.assertEquals(false, player.AddToPosition(5));
         Assert.assertEquals(5, player.GetPosition());
 
-        //loops over to 0 at 24
+        // loops over to 0 at 24
         Assert.assertEquals(true, player.AddToPosition(25));
         Assert.assertEquals(6, player.GetPosition());
     }
 
-
-        @Test
+    @Test
     public void PrisonTest() {
         Player player = new Player("NameTest", 1000);
         Assert.assertEquals(false, player.isInPrison());
@@ -73,5 +72,21 @@ public class PlayerTest {
         Assert.assertEquals(2, player.GetColor());
         Assert.assertFalse(player.SetColor(1));
 
+    }
+
+    // Test for the buyProperty method in Player
+    @Test
+    public void buyPropertyTest() {
+        Player player = new Player("ThisIsAName", 20);
+        int playerIndex = 1;
+        BoardSquare_Place propertyToBuy = new BoardSquare_Place("Candy Store", 1, 5, "Bright Blue",
+                GameData.escapeChar + "[94m");
+
+        player.SetPosition(4);
+        player.buyProperty(playerIndex, propertyToBuy);
+
+        Assert.assertEquals(propertyToBuy.GetOwner(), playerIndex);
+        Assert.assertTrue(propertyToBuy.isOwned());
+        Assert.assertEquals(player.account.GetMoney(), (20 - propertyToBuy.squarePrice));
     }
 }
